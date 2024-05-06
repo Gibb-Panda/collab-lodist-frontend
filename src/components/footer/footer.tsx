3437eec6beaa682e19dd39cd4e80e2df5a7313d3
Silva Veronique <veronique.silva@bbcag.ch>
2024-05-06 20:11:10 +0200

    Add landing page

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,3 +1,5 @@
+import React, { useEffect, useState } from "react";
+
 
 export const Footer: React.FC = () => {
     return (
---
+++ src/pages/landingPage/landingPage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const LandingPage: React.FC = () => {
+  return <h1>hello header</h1>;
+};
