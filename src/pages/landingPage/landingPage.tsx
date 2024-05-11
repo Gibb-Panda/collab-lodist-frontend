d3eba51059e2a706050840ae9e667ae80ff0b33f
Silva Veronique <veronique.silva@bbcag.ch>
2024-05-11 20:29:56 +0200

    Added image

--- src/pages/landingPage/landingPage.tsx
+++ src/pages/landingPage/landingPage.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import "./landingPage.css";
 
 export const LandingPage: React.FC = () => {
-  return <h1>I am the landing page</h1>;
+  return (
+  <>
+  {/* <div className="background"></div> */}
+  <img src="./logistics.jpg"/>
+  
+  <h1>I am the landing page</h1>;
+  </>)
 };
