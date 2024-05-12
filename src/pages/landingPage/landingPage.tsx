6009f534b1f6bfb702ae96424bdafd6e402f97c1
Silva Veronique <veronique.silva@bbcag.ch>
2024-05-12 18:53:19 +0200

    Added buttons for signup and login

--- src/pages/landingPage/landingPage.tsx
+++ src/pages/landingPage/landingPage.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import "./landingPage.css";
+import logistics from "./img/logistics.jpg";
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Button from '@mui/material/Button';
+
+
+
 
 export const LandingPage: React.FC = () => {
   return (
-  <>
-  {/* <div className="background"></div> */}
-  <img src="./logistics.jpg"/>
   
-  <h1>I am the landing page</h1>;
-  </>)
+  <div style={{ backgroundImage: `url(${logistics})` }}>
+
+  {/* <div className="background"></div> */}
+  {/* <img src="./logistics.jpg"/> */}
+
+  <h1>Collab Lodist</h1>;
+
+  <ButtonGroup variant="outlined" size="large" aria-label="Large button group" sx={{bgcolor: 'black'}}>
+      <Button>Sign up</Button>
+      <Button>Login</Button>
+    </ButtonGroup>
+  
+  </div>
+  )
 };
