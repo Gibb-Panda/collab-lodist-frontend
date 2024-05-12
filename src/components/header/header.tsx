5136bddda48bf890f1574e08219d74244ccf138d
Silva Veronique <veronique.silva@bbcag.ch>
2024-05-12 09:54:45 +0200

    Added items to hamburger menu

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,18 +6,50 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
-export default function ButtonAppBar() {
+
+
+export default function ButtonAppBar() {const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box sx={{ flexGrow: 1}}>
       <AppBar position="static" sx={{bgcolor: 'black'}}>
         <Toolbar>
+           
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose}>Persönlich</MenuItem>
+        <MenuItem onClick={handleClose}>Verwaltung</MenuItem>
+        <MenuItem onClick={handleClose}>Logistik</MenuItem>
+      </Menu>
+    
           <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
-            sx={{ mr: 2 }}
+            sx={{ mr: 2 }} id="basic-button"
+            aria-controls={open ? 'basic-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}
+
           >
             <MenuIcon />
           </IconButton>
