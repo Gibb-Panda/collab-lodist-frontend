fea792f65cecb23aaba5bc56dd1a6d1ada52e574
dahman <dahman@tie-international.com>
2024-05-13 09:24:56 +0200

    Introduce routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React, {useState} from 'react';
 import './App.css';
 import {Vehicles} from "./components/vehicles/vehicles";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Header from './components/header/header';
+import {LandingPage} from "./pages/landingPage/landingPage";
+import Footer from './components/footer/footer';
+import DashboardPage from './components/dashboardPage/dashboardPage';
 
 function App() {
-    const [value, setValue] = useState(1);
 
     return (
         <>
-            <Vehicles></Vehicles>
+            <BrowserRouter>
+                <Header/>
+                <Routes>
+                    <Route path="/" element={<LandingPage/>}></Route>
+                    <Route path="/commodities" element={<Vehicles/>}></Route>
+                    <Route path="/dashboard" element={<DashboardPage/>}></Route>
+                </Routes>
+                <Footer></Footer>
+            </BrowserRouter>
         </>
     );
 }
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -8,59 +8,66 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import {useNavigate} from "react-router-dom";
 
 
+export default function ButtonAppBar() {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const navigate = useNavigate();
 
-export default function ButtonAppBar() {const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
 
-  return (
-    <Box sx={{ flexGrow: 1}}>
-      <AppBar position="static" sx={{bgcolor: 'black'}}>
-        <Toolbar>
-           
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-      >
-        <MenuItem onClick={handleClose}>Persönlich</MenuItem>
-        <MenuItem onClick={handleClose}>Verwaltung</MenuItem>
-        <MenuItem onClick={handleClose}>Logistik</MenuItem>
-      </Menu>
-    
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }} id="basic-button"
-            aria-controls={open ? 'basic-menu' : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-            onClick={handleClick}
+    const handleSelect = (route: string) => {
+        setAnchorEl(null);
+        navigate(route);
+    };
 
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Collab Lodist
-          </Typography>
-          <Button color="inherit">Logout</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+    return (
+        <Box sx={{flexGrow: 1}}>
+            <AppBar position="static" sx={{bgcolor: 'black'}}>
+                <Toolbar>
+
+                    <Menu
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleSelect}
+                        MenuListProps={{
+                            'aria-labelledby': 'basic-button',
+                        }}
+                    >
+                        <MenuItem onClick={() => handleSelect("/dashboard")}>Dashboard</MenuItem>
+                        <MenuItem onClick={() => handleSelect("/commodities")}>Commodities</MenuItem>
+                        <MenuItem onClick={() => handleSelect("/personal")}>Persönlich</MenuItem>
+                        <MenuItem onClick={() => handleSelect("/management")}>Verwaltung</MenuItem>
+                        <MenuItem onClick={() => handleSelect("/logistics")}>Logistik</MenuItem>
+                    </Menu>
+
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{mr: 2}} id="basic-button"
+                        aria-controls={open ? 'basic-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        onClick={handleClick}
+
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                        Collab Lodist
+                    </Typography>
+                    <Button color="inherit">Logout</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 }
 
 
