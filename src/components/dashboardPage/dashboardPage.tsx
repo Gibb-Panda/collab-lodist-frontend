f6c832d65f4e8dd735cabd54743215781c693a6a
Silva Veronique <veronique.silva@bbcag.ch>
2024-05-14 22:56:36 +0200

    Styling

--- src/components/dashboardPage/dashboardPage.tsx
+++ src/components/dashboardPage/dashboardPage.tsx
@@ -1,63 +1,41 @@
 import Header from "../header/header";
-import Footer from "../footer/footer";import * as React from 'react';
+import Footer from "../footer/footer";
+import * as React from 'react';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Box from '@mui/material/Box';
 
 const buttons = [
-  <Button key="one">Persönlich</Button>,
-  <Button key="two">Verwaltung</Button>,
-  <Button key="three">Logistik</Button>,
+  <Button key="three" sx={{ bgcolor: '#fdeb8c' }}>Waren</Button>,
+  <Button key="one" sx={{ bgcolor: '#fdeb8c' }}>Persönlich</Button>,
+  <Button key="two" sx={{ bgcolor: '#fdeb8c' }}>Verwaltung</Button>,
 ];
 
 export default function GroupOrientation() {
   return (
-    <>
-    
-    <Box
-      sx={{
-        display: 'flex',
-        '& > *': {
-          m: 1,
-        },
-      }}
-    >
+    <Box sx={{ bgcolor: '#4597ff', minHeight: '100vh' }}> {/* Set background color of the page */}
       <Header />
-      <h1>I am the dashboard</h1>
-      
-      {/* <ButtonGroup orientation="vertical" aria-label="Vertical button group">
-        {buttons}
-      </ButtonGroup> */}
-      <ButtonGroup
-        orientation="vertical"
-        aria-label="Vertical button group"
-        variant="contained"
+      <h1>Menü</h1>
+
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          '& > *': {
+            m: 1,
+          },
+        }}
       >
-        {buttons}
-      </ButtonGroup>
-      {/* <ButtonGroup
-        orientation="vertical"
-        aria-label="Vertical button group"
-        variant="text"
-      >
-        {buttons}
-      </ButtonGroup> */}
+        <ButtonGroup
+          orientation="vertical"
+          aria-label="Vertical button group"
+          variant="contained"
+        >
+          {buttons}
+        </ButtonGroup>
+      </Box>
+
       <Footer />
     </Box>
-  
-    </>
   );
 }
-
-
-
-// function Dashboard() {
-
-//     return (
-//         <>
-//         <Header />
-//         <h1>I am the dashboard</h1>
-//         <Footer />
-//         </>
-//     )
-// }
